6f3403cf265d0a61e874fd1f7ee12f139f169eb0
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-11 11:23:54 +0530

    delete completed

---
+++ src/components/DeleteReg.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const DeleteReg = () => {
+  return (
+    <div className="container mt-5">
+      <div className="card shadow p-4 col-md-6 mx-auto">
+
+        <h3 className="text-center mb-3">Delete Registration</h3>
+
+        <label className="form-label">Registration Number</label>
+
+        <input
+          type="text"
+          className="form-control mb-3"
+          placeholder="Enter Registration Number"
+        />
+
+        <button className="btn btn-danger w-100">
+          Delete
+        </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default DeleteReg
